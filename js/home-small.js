25ae7265b18a76d5b6d87e8cb464927f82368442
stephenbelyea <belyea.stephen@gmail.com>
2013-08-27 17:23:51 -0400

    Rounded out

--- js/home-small.js
+++ js/home-small.js
@@ -1,96 +1,65 @@
 // Add-in no-touch class by default.
 $('body').addClass('no-touch');
+var touch  = false;
+
+// Remove hover styling class if touch event is detected.
+// Used for specific elements - site-wide.
+if ('ontouchstart' in document && !iosDevice) {
+    $('body').removeClass('no-touch');
+}
 
 $(document).ready(function(){
-
-	function defaultHomeMenu(menus){
-		console.log('default');
-		var $sub = $('.home header nav li.menu-item ul.sub-menu.show');
-		if( !menus ){
-			menus = [68,69];
-		}
-		var length = menus.length;
-		var i = 0;
-		while ( i < length ){
-			var $bg = $('.home .menu_bgs .bg_' + menus[i]);
-			var $menu = $('#menu-item-' + menus[i]);
-			$bg.css({
-				'width' : 0,
-				'height' : 0,
-				'top' : 5,
-				'left' : $menu.offset().left+30
-			});
-			i++;
-		}
-		$sub.removeClass('show');
-	}
-
-	function openHomeMenu(num){
-		closeHomeMenu();
-		var $menu = $('.home .menu_bgs .bg_' + num);
-		//var $open = $('.home .menu_bgs .bg_' + num + '.open');
-		var $sub = $('.home header nav li.menu-item-' + num + ' ul.sub-menu');
-		var pageWide = $(document).width();
-		var bgWide = pageWide * 0.95;
-		var bgFromLft = pageWide * 0.01;
-		if(num > 68){
-			bgWide = pageWide * 0.9;
-			bgFromLft = pageWide * 0.08;
-		}
-		var bgFromTop = bgWide * -0.25;
-
-		$menu.css({
-			'width' : bgWide * 1.1,
-			'height' : bgWide * 1.1,
-			'top' : bgFromTop * 1.05,
-			'left' : bgFromLft * 1.05
-		}).addClass('open');
-		var $open = $('.home .menu_bgs .bg_' + num + '.open');
-		$open.css({
-			'width' : bgWide,
-			'height' : bgWide,
-			'top' : bgFromTop,
-			'left' : bgFromLft
-		});
-
-		$sub.addClass('show');
-	}
-
-	function closeHomeMenu(){
-		var $menu = $('.home .menu_bgs .menu_bg');
-		var $sub = $('.home header nav li.menu-item ul.sub-menu.show');
-		$menu.each(function(){
-			$(this).removeClass('open');
-		});
-		defaultHomeMenu();
-	}
-
-	defaultHomeMenu();
 
 	// Remove no-touch if touch-interface is detected.
 	if ('ontouchstart' in document) {
 	    $('body').removeClass('no-touch');
+	    touch = true;
 	}
 
-	// ABOUT
-	$('#menu-item-68').live('click',function(e){
-		e.preventDefault();
-		openHomeMenu(68);
-	});
-	// WORK
-	$('#menu-item-69').live('click',function(e){
-		if ($(this).hasClass('menu-item-object-page')){	
-			console.log('sub child');
-		}
-		else{
-			console.log('main child');
-			e.preventDefault();
-			openHomeMenu(69);
-		}	
-	});
+	// Setup functions for mobile sized Home page. 
+	// Use CSS classes.
+	function getTouchMenu($this){
+		var id = $this.parent('li').attr('id');
+		id = id.replace('menu-item-','');
+		return id;
+	}; 
+	function getHoverMenu($this){
+		var id = $this.attr('id');
+		id = id.replace('menu-item-','');
+		return id;
+	};
+	function openMobileMenu(num){
+		closeMobileMenu();
+		$('.menu-item-' + num + ' .sub-menu').addClass('show').delay(200);
+		$('.menu_bgs .bg_' + num).addClass('open');
+	};
+	function closeMobileMenu(){
+		$('.menu-item .sub-menu').removeClass('show');
+		$('.menu_bgs .menu_bg').removeClass('open');
+	};
 
-	$(window).resize(function(){
-		defaultHomeMenu();
-	});
+	// Setup menu items for clickability.
+	$('#menu-item-68 a, #menu-item-69 a').addClass('tap-menu');
+	$('#menu-item-68 .sub-menu a, #menu-item-69 .sub-menu a').removeClass('tap-menu');
+
+	//if( touch ){
+		// Get initial menu item click.
+		$('#menu-item-68 a.tap-menu, #menu-item-69 a.tap-menu').live('click', function(event){
+			event.preventDefault();
+			openMobileMenu(getTouchMenu($(this)));
+		});
+	//}
+	// else{
+	// 	$('#menu-item-68 a.tap-menu, #menu-item-69 a.tap-menu').live('click', function(e){
+	// 		e.preventDefault();
+	// 		openMobileMenu(getTouchMenu($(this)));
+	// 	});
+	// 	// Get initial menu item hover.
+	// 	$('#menu-item-68, #menu-item-69').live('mouseenter', function(){
+	// 		openMobileMenu(getHoverMenu($(this)));
+	// 	}).live('mouseleave', function(){
+	// 		closeMobileMenu();
+	// 	});
+	// }
 
 });
